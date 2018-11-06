68ccb9a59d08d31b20c64116dc95eb87a28af982
João Duarte <a21702097@glzone.ulusofona.pt>
2018-11-06 15:38:21 +0000

    Added mroe examples

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -7,14 +7,23 @@ using System.Threading.Tasks;
 namespace ExceptionTest {
     class Program {
         static void Main(string[] args) {
+            int i = 0;
             Console.Write("Insere um membro inteiro: ");
 
             try {
-                int i = Convert.ToInt32(Console.ReadLine());
+                i = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Número inserido: {i}");
             }
+            catch (OverflowException) {
+                double doublee = Convert.ToDouble(i);
+                Console.WriteLine("Agora é um double");
+            }
+            catch (FormatException) {
+                string str = Convert.ToString(i);
+                Console.WriteLine("Agora é uma string");
+            }
             catch (Exception e) {
-                Console.WriteLine("Too bad! Error: " + e.Message);
+                Console.WriteLine("Bum! Erro: " + e.Message);
             }
         }
     }
